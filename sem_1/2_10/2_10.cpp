25a6db3cb53e404fdb612e698caad301541445ba
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:37:47 +0500

    Add files via upload

---
+++ sem_1/2_10/2_10.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int a;
+	int max = 0;
+	int min = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		if (a >= max)
+		{
+			max = a;
+		}
+		else if (a <= min)
+		{
+			min = a;
+		}
+		if (max < min)
+		{
+			min = max;
+		}
+		else if (min > max)
+		{
+			max = min;
+		}
+
+	}
+	cout << max << endl << min;
+	return 0;
+}
