429e42f1b8cac443ec298c0903c127db12a89ae0
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:24:53 +0500

    Add files via upload

---
+++ sem_1/2_3/2_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, sum;
+	cin >> n;
+	sum = 0;
+	if (n <= 0)
+	{
+		cout << "так нельзя" << endl;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			int object = 1;
+			for (int j = i; j <= 2 * i; j++)
+			{
+				object *= j;
+			}
+			sum += object;
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
