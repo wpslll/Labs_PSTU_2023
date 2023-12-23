b6f06acac2b39dc4f4d814d26f84f4651ed63cef
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 19:45:12 +0500

    Add files via upload

---
+++ sem_1/1_2/1_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b;
+	cin >> a >> b;
+	if (b == 0)
+	{
+		cout << "На ноль делить нельзя";
+	}
+	else
+	{
+		cout << a / b;  
+	}
+	return 0;
+}
