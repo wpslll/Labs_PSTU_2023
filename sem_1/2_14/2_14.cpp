160cf42033eb88de866fdb064d4c70f49baab633
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:43:16 +0500

    Add files via upload

---
+++ sem_1/2_14/2_14.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int sum = 0;
+	int k = 0;
+	while (n != 0)
+	{
+		k = n % 10;
+		sum = sum + k;
+		n = n / 10;
+	}
+	cout << sum;
+	return 0;
+}
