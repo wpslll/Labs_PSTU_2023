564e1a91c071b4bcbbab3ef1c247a5f6b04e646e
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:48:47 +0500

    Add files via upload

---
+++ sem_1/2_17/2_17.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	float x;
+	float y = 1;
+	int a;
+	cin >> n;
+	cin >> x;
+	for (int i = 1; i < n; i++)
+	{
+		a = 1;
+		for (int j = 1; j < i; j++)
+		{
+			a = a * (a + 1);
+		}
+		y = y + pow(x, i) / (a);
+	}
+	cout << y;
+	return 0;
+}
