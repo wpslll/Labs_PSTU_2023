4cadeaa4c1748734503abba3a4b12d6cf74846dc
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:32:08 +0500

    Add files via upload

---
+++ sem_1/2_7/2_7_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= sqrt(n); i++)
+	{
+		for (int j = 1; j <= sqrt(n); j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
