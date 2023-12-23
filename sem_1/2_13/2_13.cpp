b7c3a79587bfb36d97ef3a4c5d5f282ca162f11c
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:41:27 +0500

    Add files via upload

---
+++ sem_1/2_13/2_13.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int c;
+	float x;
+	float xa;
+	float xb;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	int d = b * b + (-4 * a * c);
+	if (d > 0)
+	{
+		x = (-b + sqrt(d)) / (2 * a);
+		xa = (-b - sqrt(d)) / (2 * a);
+		cout << x << endl <<  xa;
+	}
+	if (d < 0)
+	{
+		cout << "discriminant < 0";
+	}
+	if ( d == 0 )
+	{
+		xb = -b / (2 * a);
+		cout << xb;
+	}
+	return 0;
+}
