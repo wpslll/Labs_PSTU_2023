5718c96f2115028de3c3b92924dd092e39f83a79
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:47:06 +0500

    Add files via upload

---
+++ sem_1/2_16/2_16.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			sum = sum + i;
+		}
+		if (i % 2 != 0)
+		{
+			sum = sum - i;
+		}
+	}
+	cout << sum;
+	return 0;
+}
