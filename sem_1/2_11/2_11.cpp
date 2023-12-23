209de45ac3edf2cf0c9dbac58da3b6fcb20eb428
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:39:00 +0500

    Add files via upload

---
+++ sem_1/2_11/2_11.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int a;
+	bool minus = false;
+	bool plus = false;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		if (!minus && !plus)
+		{
+			if (a < 0) { minus = true; }
+			else if (a > 0) { plus = true; }
+		}
+	}
+	if (minus)
+	{
+		cout << "first number is negative";
+	}
+	else if (plus)
+	{
+		cout << "first number is positive";
+	}
+	else
+	{
+		cout << "every number is 0";
+	}
+	return 0;
+}
