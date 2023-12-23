3c75223f9f60f06d2395a3ed8b63bb632c157f41
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:45:30 +0500

    Add files via upload

---
+++ sem_1/2_15/2_15.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int s;
+	cin >> s;
+	while (n != 0)
+	{
+		if (n % 10 == s)
+		{
+			cout << "there is number " << s;
+		}
+		n = n / 10;
+	}
+	return 0;
+}
