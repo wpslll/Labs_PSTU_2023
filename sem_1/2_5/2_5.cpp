5cdefbbb0a9b72ee1a54917a8bf595ed8fc40a23
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-23 21:29:20 +0500

    Add files via upload

---
+++ sem_1/2_5/2_5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int o = n - 1;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= o; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+		o--;
+		cout << endl;
+	}
+	return 0;
+}
