2819f521eabc66128237721833ae024c303531af
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-24 15:50:08 +0500

    Add files via upload

---
+++ sem_1/2_19/2_19.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int s = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		if (i % 3 != 0)
+		{
+			s += i;
+		}
+		else if (i % 3 == 0)
+		{
+			s -= i;
+		}
+	}
+	cout << s;
+	return 0;
+}
