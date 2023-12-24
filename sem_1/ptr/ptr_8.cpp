49412b4f840ed31c167b0a9b5ba33cf33fef5019
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-24 17:14:12 +0500

    Add files via upload

---
+++ sem_1/ptr/ptr_8.cpp
@@ -0,0 +1,28 @@
+﻿﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+	string a, b, c;
+	cin >> a >> b >> c;
+	int x = 0, y = 0, z = 0;
+	int l = a.length();
+	for (int i = 0; i < l; i++)
+	{
+		string sum = a.substr(i, 1);
+		if (sum == a)
+		{
+			x++;
+		}
+		else if (sum == b)
+		{
+			y++;
+		}
+		else
+		{
+			z++;
+		}
+	}
+	cout << x << "  " << y << "  " << z << "  " << a.length();
+	return 0;
+}
