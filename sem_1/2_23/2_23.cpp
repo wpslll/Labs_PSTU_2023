5d597539a084b02ec8b945e7091f85d9aad0d301
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-24 16:43:28 +0500

    Add files via upload

---
+++ sem_1/2_23/2_23.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a, max;
+	int n;
+	int b = 0;
+	cin >> n;
+	int i = 1;
+	max = sin(n + i / n);
+	while (i <= n)
+	{
+		i++;
+		float a = sin(n + i / n);
+		if (a == max)
+		{
+			b++;
+		}
+	}
+	cout << b;
+	return 0;
+}
