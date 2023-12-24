dd91491b65773370f4529671b673807be27dd02d
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-24 16:43:04 +0500

    Add files via upload

---
+++ sem_1/2_22/2_22.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	float a;
+	cin >> n;
+	int i = 2;
+	int num = 2;
+	float max = sin(n + i / n);
+	cout << max<< endl;
+	while (i <= n)
+	{
+		a = sin(n + i / n);
+		cout << a << endl;
+		if (a > max)
+		{
+			num = i;
+			max = a;
+		}
+		i++;
+	}
+	cout << "the biggest number is" << num << "  " << max;
+	return 0;
+}
