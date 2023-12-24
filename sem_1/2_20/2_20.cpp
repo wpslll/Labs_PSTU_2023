f28f5ab4a12f1804626f80834b10e564ef48c5da
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-24 15:52:08 +0500

    Add files via upload

---
+++ sem_1/2_20/2_20.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float n, s;
+	cin >> n >> s;
+	int i = 1;
+	float a;
+	bool f = false;
+	while (i <= n && !f)
+	{
+		a = sin(n + i / n);
+		a = round(a * 10) / 10;
+		cout << n + i / n << "|" << a << endl;
+		if (a == s)
+		{
+			f = true;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	if (f == true)
+	{
+		cout << "s exists";
+	}
+	else
+	{
+		cout << "no s";
+	}
+	return 0;
+}
