f420a241eb1031a580ff918c1ad3bc35058a3869
wpslll <151571121+wpslll@users.noreply.github.com>
2023-12-24 16:30:42 +0500

    Add files via upload

---
+++ sem_1/2_21/2_21.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	bool a = true;
+	int n;
+	int b = INT_MIN;
+	do
+	{
+		cin >> n;
+		if (n == 0)
+		{
+			b = 0;
+		}
+		if (b > n)
+		{
+			a = false;
+		}
+		b = n;
+	} while (n != 0);
+	if (a)
+	{
+		cout << "ordered";
+	}
+	else
+	{
+		cout << "not ordered";
+	}
+	return 0;
+}
