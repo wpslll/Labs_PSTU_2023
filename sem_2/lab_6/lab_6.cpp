d6460012c954bd0514cbdc681681f8aa350ef47d
wpslll <151571121+wpslll@users.noreply.github.com>
2024-02-20 20:07:03 +0500

    Add files via upload

---
+++ sem_2/lab_6/lab_6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int r, n = 20;
+	char c[20];
+	fgets(c, sizeof(c), stdin);
+	for (int i = 0; i < n; i++)
+	{
+		if (c[i] == '~' || c[i] == '!' || c[i] == '@' || c[i] == '#' || c[i] == '$' || c[i] == '%' || c[i] == '^' || c[i] == '&' || c[i] == '*' || c[i] == '(' || c[i] == ')' || c[i] == '-' || c[i] == '+' || c[i] == '_' || c[i] == '=' || c[i] == '`' || c[i] == '/' || c[i] == '.' || c[i] == ',' || c[i] == '?' || c[i] == '"' || c[i] == ':' || c[i] == ';' || c[i] == ']' || c[i] == '[' || c[i] == '{' || c[i] == '}' || c[i] == '|' || c[i] == '№')
+		{
+			c[i] = 0;
+		}
+	}
+	for (int i = 0; i < 20; i++)
+	{
+		cout << c[i];
+	}
+	return 0;
+}
