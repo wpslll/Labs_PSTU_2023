b3c16e92c6afb703e76c47948465cb5bcffb1fd9
wpslll <151571121+wpslll@users.noreply.github.com>
2024-02-20 20:04:12 +0500

    Add files via upload

---
+++ sem_2/lab_3_2/lab_3_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int f(int n)
+{
+	if (n == 1 || n == 2)
+	{
+		return 1;
+	}
+	if (n == 0)
+	{
+		return 0;
+	}
+	return f(n - 1) + f(n - 2);
+}
+int main()
+{
+	int t;
+	cin >> t;
+	cout << f(t) << " ";
+	return 0;
+}
