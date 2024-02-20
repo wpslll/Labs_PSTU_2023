9524a879ab2b57aae50bd7cbb3632fd29b170adf
wpslll <151571121+wpslll@users.noreply.github.com>
2024-02-20 20:07:29 +0500

    Add files via upload

---
+++ sem_2/lab_7/lab_7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+void s(float n, int a)
+{
+	a = pow(a, n);
+	cout << a;
+}
+void s (float n, double a)
+{
+	a = pow(a, 1 / n);
+	cout << a;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	s(n, 0.36);
+	return 0;
+}
