dbd620b51499a87b1d9940c5400925fcfaac00e4
wpslll <151571121+wpslll@users.noreply.github.com>
2024-02-20 20:04:38 +0500

    Add files via upload

---
+++ sem_2/lab_3_3/lab_3_3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void f(int n, int i, int j)
+{
+	if (n == 1)
+	{
+		cout << "moving circle " << n << " from pin " << i << " to pin " << j << endl;
+	}
+	else
+	{
+		int tmp = 6 - i - j;
+		f(n - 1, i, tmp);
+		cout << "moving circle " << n << " from pin " << i << " to pin " << j << endl;
+		f(n - 1, tmp, i);
+	}
+}
+int main()
+{
+	f(3, 1, 2);
+	return 0;
+}
