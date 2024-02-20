b2198e5c284478bf26d33b29664e406ba35cf207
wpslll <151571121+wpslll@users.noreply.github.com>
2024-02-20 20:03:28 +0500

    Add files via upload

---
+++ sem_2/lab_2/lab_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a[5][5];
+	int n = 5, k = 1, r = 4, c;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		c = i + 1;
+		for (int j = 0; j < k; j++)
+		{
+			a[i][j] = c;
+			c--;
+		}
+		k++;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j];
+		}
+		cout << endl;
+	}
+	return 0;
+}
